fcb1785f364ca65020b5b6df77e038347379687b
chandlerlei2017 <chandlerlei87@gmail.com>
2019-10-09 08:02:14 -0400

    name

---
+++ src/pages/Home/components/IntroPane/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+class IntroPane extends React.PureComponent {
+  render() {
+    return(
+      <div className="intro">
+        <h1>Chandler Lei</h1>
+      </div>
+    );
+  }
+}
+
+export default IntroPane;
---
+++ src/pages/Home/components/Particle/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import './index.css'
+
+const particleParams = {
+  "particles": {
+    "number": {
+      "value": 400,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.6,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 3,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.5,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 3,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  // "interactivity": {
+  //   "detect_on": "canvas",
+  //   "events": {
+  //     "onhover": {
+  //       "enable": true,
+  //       "mode": "bubble"
+  //     },
+  //     "resize": true
+  //   },
+  //   "modes": {
+  //     "grab": {
+  //       "distance": 400,
+  //       "line_linked": {
+  //         "opacity": 1
+  //       }
+  //     },
+  //     "bubble": {
+  //       "distance": 100,
+  //       "size": 2,
+  //       "duration": 3,
+  //       "opacity": 1,
+  //       "speed": 3
+  //     },
+  //     "repulse": {
+  //       "distance": 200,
+  //       "duration": 0.4
+  //     },
+  //     "push": {
+  //       "particles_nb": 4
+  //     },
+  //     "remove": {
+  //       "particles_nb": 2
+  //     }
+  //   }
+  // },
+  "retina_detect": true
+};
+
+function Particle() {
+  return <Particles  className="particles" params={particleParams}/>
+}
+
+export default Particle;
