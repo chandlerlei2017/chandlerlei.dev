828e4d3cce4c3be6c1fbc85bdf8367a7379d7997
chandlerlei2017 <chandlerlei87@gmail.com>
2019-10-11 07:36:15 -0400

    anime init

--- src/pages/Home/components/IntroPane/index.js
+++ src/pages/Home/components/IntroPane/index.js
@@ -3,14 +3,36 @@ import './index.css'
 import Avatar from '../../../../shared/Avatar/index'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMusic, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import anime from 'animejs/lib/anime.es.js';
 
 class IntroPane extends React.PureComponent {
+  componentDidMount() {
+    var tl = anime.timeline({
+      easing: 'easeInOutExpo',
+      duration: 1000,
+    });
+
+    tl
+    .add({
+      targets: '.image-cropper',
+      opacity: [0, 100],
+    }, 200)
+    .add({
+      targets: '.animate',
+      opacity: [0, 100],
+    }, 400) // relative offset
+    .add({
+      targets: '.animate2',
+      opacity: [0, 100],
+    }, 600); // absolute offset
+  }
+
   render() {
     return (
       <div className="intro" align="center" >
         <Avatar styles={{ height: "150px", width: "150px" }} />
-        <h1 className="mt-5">Chandler Lei - Developer</h1>
-        <h2 className="mt-5 mb-5">
+        <h1 className="mt-5 animate">Chandler Lei - Developer</h1>
+        <h2 className="mt-5 mb-5 animate2">
           <FontAwesomeIcon icon={faMusic} />
           {} Starry, starry night - Paint your palette blue and grey {}
           <FontAwesomeIcon icon={faMusic} />
