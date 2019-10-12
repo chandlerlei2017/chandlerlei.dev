2788ac3f0e27c15f15c4af71f83d515ea9a44f23
chandlerlei2017 <chandlerlei87@gmail.com>
2019-10-12 15:24:02 -0400

    temp animate

--- src/pages/Home/components/About/components/AnimationContainer/index.js
+++ src/pages/Home/components/About/components/AnimationContainer/index.js
@@ -15,6 +15,11 @@ class AnimationContainer extends React.PureComponent {
     this.state= {
       firstBubble: "",
       secondBubble: "",
+      thirdBubble: "",
+      fourthBubble: "",
+      fifthBubble: "",
+      sixthBubble: "",
+      seventhBubble: "",
     }
   }
 
@@ -25,9 +30,11 @@ class AnimationContainer extends React.PureComponent {
   }
 
   componentDidMount() {
+    // Bubble1
+
     new ScrollMagic.Scene({
-      triggerElement: "#bubble1",
-      duration: 400,
+      triggerElement: "#trigger1",
+      duration: 200,
     })
     .setPin("#bubble1")
     .addTo(this.controller)
@@ -42,11 +49,12 @@ class AnimationContainer extends React.PureComponent {
       }
     });
 
+    // Bubble2
+
     new ScrollMagic.Scene({
-      triggerElement: "#bubble2",
-      duration: 400,
+      triggerElement: "#trigger2",
+      duration: 200,
     })
-    .setPin("#bubble1")
     .setPin("#bubble2")
     .addTo(this.controller)
     .on("leave", (event) => {
@@ -59,16 +67,188 @@ class AnimationContainer extends React.PureComponent {
         this.handleScroll("secondBubble","");
       }
     });
+
+    let pin1 = new ScrollMagic.Scene({
+      triggerElement: "#trigger2",
+      duration: 0,
+    })
+    .setPin("#trigger1")
+    .addTo(this.controller);
+
+    // Bubble3
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger3",
+      duration: 200,
+    })
+    .setPin("#bubble3")
+    .addTo(this.controller)
+    .on("leave", (event) => {
+      if (event.scrollDirection === "FORWARD") {
+        this.handleScroll("thirdBubble","I am currently in 3rd year, studying Computer Engineering at the University of Waterloo");
+      }
+    })
+    .on("enter", (event) => {
+      if (event.scrollDirection === "REVERSE") {
+        this.handleScroll("thirdBubble","");
+      }
+    });
+
+    let pin2 = new ScrollMagic.Scene({
+      triggerElement: "#trigger3",
+      duration: 0,
+    })
+    .setPin("#trigger2")
+    .addTo(this.controller);
+
+    // Bubble4
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger4",
+      duration: 200,
+    })
+    .setPin("#bubble4")
+    .addTo(this.controller)
+    .on("leave", (event) => {
+      if (event.scrollDirection === "FORWARD") {
+        this.handleScroll("fourthBubble",
+        <>
+          <p>Some quick facts about me:</p>
+          <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} As you likely guessed, I'm named after Chandler Bing from "Friends"</p>
+          <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I was born in Stockholm, Sweden</p>
+          <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I eat hotdogs and fries without any toppings </p>
+          <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I don't think pineapples belong on pizza, sorry (not sorry))
+        </>);
+      }
+    })
+    .on("enter", (event) => {
+      if (event.scrollDirection === "REVERSE") {
+        this.handleScroll("fourthBubble","");
+      }
+    });
+
+    let pin3 = new ScrollMagic.Scene({
+      triggerElement: "#trigger4",
+      duration: 0,
+    })
+    .setPin("#trigger3")
+    .addTo(this.controller);
+
+    // Bubble5
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger5",
+      duration: 200,
+    })
+    .setPin("#bubble5")
+    .addTo(this.controller)
+    .on("leave", (event) => {
+      if (event.scrollDirection === "FORWARD") {
+        this.handleScroll("fifthBubble","Great! Now you know a bit about me.");
+      }
+    })
+    .on("enter", (event) => {
+      if (event.scrollDirection === "REVERSE") {
+        this.handleScroll("fifthBubble","");
+      }
+    });
+
+    let pin4 = new ScrollMagic.Scene({
+      triggerElement: "#trigger5",
+      duration: 0,
+    })
+    .setPin("#trigger4")
+    .addTo(this.controller);
+
+    // Bubble6
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger6",
+      duration: 200,
+    })
+    .setPin("#bubble6")
+    .addTo(this.controller)
+    .on("leave", (event) => {
+      if (event.scrollDirection === "FORWARD") {
+        this.handleScroll("sixthBubble","And hopefully you don't hate me for my hawaiian pizza comment 😅");
+      }
+    })
+    .on("enter", (event) => {
+      if (event.scrollDirection === "REVERSE") {
+        this.handleScroll("sixthBubble","");
+      }
+    });
+
+    let pin5 = new ScrollMagic.Scene({
+      triggerElement: "#trigger6",
+      duration: 0,
+    })
+    .setPin("#trigger5")
+    .addTo(this.controller);
+
+    // Bubble7
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger7",
+      duration: 200,
+    })
+    .setPin("#bubble7")
+    .addTo(this.controller)
+    .on("leave", (event) => {
+      if (event.scrollDirection === "FORWARD") {
+        this.handleScroll("seventhBubble","Anyway, Here's a bit more about what I do...");
+        pin1.removePin();
+        pin2.removePin();
+        pin3.removePin();
+        pin4.removePin();
+        pin5.removePin();
+        pin6.removePin();
+      }
+    })
+    .on("enter", (event) => {
+      if (event.scrollDirection === "REVERSE") {
+        this.handleScroll("seventhBubble","");
+        pin1.setPin("#trigger1");
+        pin2.setPin("#trigger2");
+        pin3.setPin("#trigger3");
+        pin4.setPin("#trigger4");
+        pin5.setPin("#trigger5");
+        pin6.setPin("#trigger6");
+      }
+    });
+
+    let pin6 = new ScrollMagic.Scene({
+      triggerElement: "#trigger7",
+      duration: 0,
+    })
+    .setPin("#trigger6")
+    .addTo(this.controller);
   }
 
   render() {
     return(
       <>
-        <AnimatedBubble id="bubble1">{this.state.firstBubble}</AnimatedBubble>
-        <AnimatedBubble id="bubble2">{this.state.secondBubble}</AnimatedBubble>
-        <AnimatedBubble id="animate-2"/>
-        <AnimatedBubble id="animate-2"/>
-        <AnimatedBubble id="animate-2"/>
+        <div id="trigger1">
+          <AnimatedBubble id="bubble1">{this.state.firstBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger2">
+          <AnimatedBubble id="bubble2">{this.state.secondBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger3">
+          <AnimatedBubble id="bubble3">{this.state.thirdBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger4">
+          <AnimatedBubble id="bubble4">{this.state.fourthBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger5">
+          <AnimatedBubble id="bubble5">{this.state.fifthBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger6">
+          <AnimatedBubble id="bubble6">{this.state.sixthBubble}</AnimatedBubble>
+        </div>
+        <div id="trigger7">
+          <AnimatedBubble id="bubble7">{this.state.seventhBubble}</AnimatedBubble>
+        </div>
       </>
     );
   }
--- src/pages/Home/components/About/index.js
+++ src/pages/Home/components/About/index.js
@@ -5,68 +5,6 @@ import AnimationContainer from './components/AnimationContainer/index'
 
 
 class AboutView extends React.PureComponent {
-  // constructor(props) {
-  //   super(props);
-  //   this.controller = new ScrollMagic.Controller();
-  //   this.state = {
-  //     firstBubble: <Ellipses/>
-  //   }
-  //   this.handleScroll = this.handleScroll.bind(this)
-  //   this.addAnimation = this.addAnimation.bind(this)
-  //   this.timeline = anime.timeline({
-  //     easing: 'cubicBezier(.5, .05, .1, .3)',
-  //     duration: 300,
-  //     direction: 'alternate',
-  //     loop: true,
-  //   });
-  // }
-
-  // handleScroll(name, message) {
-  //   this.setState({
-  //     [name]: message
-  //   });
-  // }
-  // addAnimation() {
-  //   this.timeline
-  //   .add({
-  //     targets: '#circle-1',
-  //     // opacity: [0, 100],
-  //     translateY: -10
-  //   }, 100)
-  //   .add({
-  //     targets: '#circle-2',
-  //     // opacity: [0, 100],
-  //     translateY: -10
-  //   }, 200) // relative offset
-  //   .add({
-  //     targets: '#circle-3',
-  //     // opacity: [0, 100],
-  //     translateY: -10
-  //   }, 300); // absolute offset
-  // }
-
-  componentDidMount() {
-    // this.addAnimation();
-    // new ScrollMagic.Scene({
-    //   triggerElement: "#animate-1",
-    //   duration: 400,
-    // })
-    // .setPin("#animate-1")
-    // .addTo(this.controller)
-    // .on("leave", (event) => {
-    //   if (event.scrollDirection === "FORWARD") {
-    //     this.handleScroll("firstBubble","Hi, I'm Chandler!");
-    //   }
-    // })
-    // .on("enter", (event) => {
-    //   if (event.scrollDirection === "REVERSE") {
-    //     this.handleScroll("firstBubble",<Ellipses/>);
-    //     this.addAnimation();
-    //   }
-    // });
-
-
-  }
 
   render() {
     const styles = {
@@ -85,21 +23,7 @@ class AboutView extends React.PureComponent {
         </div>
         <div className="col-10 col-sm-6">
           <AnimationContainer/>
-          {/* <Bubble id="animate-1">{this.state.firstBubble}</Bubble>
-          <Bubble>Thanks for visiting my website!</Bubble>
-          <Bubble>I am currently in 3rd year, studying Computer Engineering at the University of Waterloo</Bubble>
-          <Bubble>
-            <p>Some quick facts about me:</p>
-            <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} As you likely guessed, I'm named after Chandler Bing from "Friends"</p>
-            <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I was born in Stockholm, Sweden</p>
-            <p> <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I eat hotdogs and fries without any toppings </p>
-            <FontAwesomeIcon icon={faLongArrowAltRight} />{' '} I don't think pineapples belong on pizza, sorry (not sorry)
-          </Bubble>
-          <Bubble>
-            Great! Now you know a bit about me.
-          </Bubble>
-          <Bubble>And hopefully you don't hate me for my hawaiian pizza comment 😅 </Bubble>
-          <Bubble>Anyway, Here's a bit more about what I do...</Bubble> */}
+          <div style={{height: "2000px"}}/>
         </div>
       </div>
     );
