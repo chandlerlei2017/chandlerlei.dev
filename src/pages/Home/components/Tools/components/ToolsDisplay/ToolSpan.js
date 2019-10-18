2ed51c602296752a2772ee191d4ca214175356bc
chandlerlei2017 <chandlerlei87@gmail.com>
2019-10-17 21:37:06 -0400

    fix contexts

--- src/pages/Home/components/Tools/components/ToolsDisplay/ToolSpan.js
+++ src/pages/Home/components/Tools/components/ToolsDisplay/ToolSpan.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './index.css';
+import { selectedContext } from '../../selectedContext'
 
 function ToolSpan(props) {
   return(
-    <span className="ml-4 mr-4 mb-3">{props.children}</span>
+    <selectedContext.Consumer>
+      {({selected}) => (
+        <span className="ml-4 mr-4 mb-3">{props.children}</span>
+      )}
+    </selectedContext.Consumer>
   );
 }
 
