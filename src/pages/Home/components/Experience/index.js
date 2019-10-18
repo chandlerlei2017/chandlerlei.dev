2df184ead6822005faad5ed0ad050d85be0d86b8
chandlerlei2017 <clei@secretmission.io>
2019-10-18 17:43:24 -0400

    init experience section

--- src/pages/Home/components/Experience/index.js
+++ src/pages/Home/components/Experience/index.js
@@ -5,8 +5,8 @@ class ExperienceView extends React.PureComponent {
 
   render() {
     return (
-      <div style={{height: "500px"}}>
-        Experience Section
+      <div className="experience-section pt-5" align="center" style={{ height: "500px" }}>
+        <h2 className="mb-5">My Past Work</h2>
       </div>
     );
   }
