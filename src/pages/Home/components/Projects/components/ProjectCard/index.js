fb56b5e8cf6ec1c13007f4187e401362713f2ae4
chandlerlei2017 <chandlerlei87@gmail.com>
2019-10-24 07:39:26 -0400

    project card update

--- src/pages/Home/components/Projects/components/ProjectCard/index.js
+++ src/pages/Home/components/Projects/components/ProjectCard/index.js
@@ -6,7 +6,7 @@ class ProjectCard extends React.PureComponent {
   render() {
     return(
         <div className="col-md-4 p-3">
-          <div className={`rounded ${this.props.image} project-card`} style={{background: "", height: "250px"}}>
+          <div className={`${this.props.image} project-card`} style={{background: "", height: "250px"}}>
             <div className="project-text">
 
             </div>
--- src/pages/Home/components/Projects/index.js
+++ src/pages/Home/components/Projects/index.js
@@ -2,6 +2,51 @@ import React from 'react';
 import './index.css'
 import ProjectCard from './components/ProjectCard/index'
 
+const projectData = [
+  {
+    image: "spotify-tracks",
+    title: "Spotify Tracks",
+    github: "https://github.com/chandlerlei2017/Spotify-Plays",
+    url: "spotify-tracks.com",
+    description: "Some description"
+  },
+  {
+    image: "uwsublet",
+    title: "UWSublet",
+    github: "https://github.com/chandlerlei2017/sublet",
+    url: "",
+    description: ""
+  },
+  {
+    image: "personal-website",
+    title: "chandlerlei.dev",
+    github: "https://github.com/chandlerlei2017/chandlerlei.dev",
+    url: "chandlerlei.dev",
+    description: "Some description"
+  },
+  {
+    image: "hardware",
+    title: "Bike Safe",
+    github: "",
+    url: "",
+    description: "Some description"
+  },
+  {
+    image: "1",
+    title: "",
+    github: "",
+    url: "",
+    description: ""
+  },
+  {
+    image: "2",
+    title: "",
+    github: "",
+    url: "",
+    description: ""
+  },
+]
+
 class ProjectsView extends React.PureComponent {
   render() {
     return(
@@ -9,12 +54,11 @@ class ProjectsView extends React.PureComponent {
         <h2 className="mb-5">My Past Work</h2>
         <div className="col-md-12 col-xxl-8">
           <div className="row" style={{margin: "0"}}>
-            <ProjectCard image="spotify-plays"/>
-            <ProjectCard image="uwsublet"/>
-            <ProjectCard image="personal-website"/>
-            <ProjectCard image="hardware"/>
-            <ProjectCard image=""/>
-            <ProjectCard iamge=""/>
+            {projectData.map(project => {
+              return (
+                <ProjectCard key={project.image} image={project.image}/>
+              );
+            })}
           </div>
         </div>
       </div>
